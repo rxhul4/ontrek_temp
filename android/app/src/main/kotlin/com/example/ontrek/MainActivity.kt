3b34128bc066c00dc019dad461ffe3dde0f53f4f
Rahul Solanki <rahul.solanki0147@gmail.com>
2024-01-25 14:18:34 +0530

    done

--- android/app/src/main/kotlin/com/example/ontrek/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ontrek/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.ontrek
 
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+//class MainActivity: FlutterActivity() {
+//}
+
+class MainActivity: FlutterFragmentActivity() {
+
 }
