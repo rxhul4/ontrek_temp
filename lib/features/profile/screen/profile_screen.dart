92b902ea51d967ed9c89a22eab101e9de4d7db8b
672022 <sagartrada5@gmail.com>
2024-01-31 19:10:01 +0530

    Task list screen 31-01 EOD

--- lib/features/profile/screen/profile_screen.dart
+++ lib/features/profile/screen/profile_screen.dart
@@ -5,7 +5,8 @@ import 'package:ontrek/core/utils/widget_utils.dart';
 
 class ProfileScreen extends StatefulWidget {
   ScrollController? scrollController;
-  ProfileScreen({super.key,this.scrollController});
+
+  ProfileScreen({super.key, this.scrollController});
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
@@ -13,7 +14,8 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   DraggableScrollableController draggableScrollableController =
-  DraggableScrollableController();
+      DraggableScrollableController();
+
   @override
   Widget build(BuildContext context) {
     return Animate(
@@ -21,7 +23,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ScaleEffect(
             curve: Curves.bounceIn,
             begin: Offset(0, -10),
-
             duration: Duration(milliseconds: 100)),
       ],
       child: Container(
@@ -35,20 +36,202 @@ class _ProfileScreenState extends State<ProfileScreen> {
               ),
             ],
             borderRadius:
-            WidgetUtils.borderRadiousonly(topright: 18, topleft: 18),
+                WidgetUtils.borderRadiousonly(topright: 18, topleft: 18),
             color: AppConstant.whiteColor),
-        child: ListView.builder(
-          itemCount: 20,
-          shrinkWrap: true,
-          controller: widget.scrollController,
-          itemBuilder: (context, index) {
-            return Center(
-              child: WidgetUtils.commonTextWidget(
-                  text: "text", textColor: Colors.red),
-            );
-          },
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              controller: widget.scrollController,
+              physics: NeverScrollableScrollPhysics(),
+              child: WidgetUtils.commonContainer(
+                height: MediaQuery.of(context).size.height / 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      alignment: Alignment.topRight,
+                      children: [
+                        WidgetUtils.commonContainer(
+                            height: 80,
+                            width: 80,
+                            decoration: WidgetUtils.commonBoxDecoration(
+                                shape: BoxShape.circle,
+                                color: AppConstant.greyColor.withOpacity(0.3)),
+                            child: Icon(
+                              Icons.person,
+                              size: 40,
+                              color: AppConstant.greyColor,
+                            )),
+                        WidgetUtils.commonContainer(
+                          padding: EdgeInsets.all(5),
+                          decoration: WidgetUtils.commonBoxDecoration(
+                              color: Colors.white, shape: BoxShape.circle),
+                          child: Icon(Icons.edit, size: 15),
+                        )
+                      ],
+                    ),
+                    WidgetUtils.commonSizedBox(height: 10),
+                    WidgetUtils.commonTextWidget(
+                        text: 'Vatsal',
+                        textColor: AppConstant.blackColor,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18),
+                    WidgetUtils.commonTextWidget(
+                        text: 'Epist Interior Pvt Ltd',
+                        textColor: AppConstant.blackColor.withOpacity(0.6),
+                        fontSize: 12),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border:
+                      Border(bottom: BorderSide(color: AppConstant.greyColor))),
+              child: ListTile(
+                contentPadding: EdgeInsets.only(left: 10, right: 10),
+                leading: WidgetUtils.commonContainer(
+                    padding: WidgetUtils.edgeInsetsAll(allPadding: 10),
+                    // margin: WidgetUtils.edgeInsetsOnly(left: 20, right: 20),
+                    decoration: WidgetUtils.commonBoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                            color: AppConstant.greyColor.withOpacity(0.5)),
+                        color: AppConstant.greyColor.withOpacity(0.3)),
+                    child: Icon(
+                      Icons.person,
+                      size: 30,
+                      color: AppConstant.greyColor,
+                    )),
+                title: WidgetUtils.commonTextWidget(
+                    text: 'Kenil Patel',
+                    textColor: AppConstant.blackColor,
+                    fontWeight: FontWeight.w500),
+                subtitle: WidgetUtils.commonTextWidget(
+                    text: 'Reporting Manager',
+                    textColor: AppConstant.blackColor.withOpacity(0.6),
+                    fontSize: 12),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.call, color: AppConstant.blueColor),
+                ),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    ListView.builder(
+                      itemCount: moreOptionsName.length,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return moreOptions(
+                          icon: moreOptionIcons[index],
+                          text: moreOptionsName[index],
+                          index: index,
+                          itemCount: moreOptionsName.length,
+                        );
+                      },
+                    ),
+                    Divider(),
+                    Text('version')
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  List<String> moreOptionsName = [
+    "Reimbursement",
+    "Settings",
+    "Help",
+    "FAQ",
+    "Terms & Conditions",
+    "Privacy Policy",
+    "App Settings",
+    "Sign Out",
+  ];
+  List<IconData> moreOptionIcons = [
+    Icons.monetization_on,
+    Icons.settings,
+    Icons.chat,
+    Icons.help_outline_outlined,
+    Icons.verified,
+    Icons.privacy_tip,
+    Icons.app_settings_alt,
+    Icons.logout,
+  ];
+
+  Widget moreOptions({
+    IconData? icon,
+    required String text,
+    required int index,
+    required int itemCount,
+  }) {
+    return Column(
+      children: [
+        ListTile(
+          contentPadding: EdgeInsets.only(left: 10),
+          leading: Icon(icon),
+          title: WidgetUtils.commonContainer(
+            // decoration: WidgetUtils.commonBoxDecoration(
+            //     border: Border(
+            //         bottom: BorderSide(
+            //   color: AppConstant.greyColor.withOpacity(0.5),
+            //   width: 1,
+            // ))),
+            child: WidgetUtils.commonTextWidget(
+                text: text, textColor: AppConstant.blackColor),
+          ),
+        ),
+        if(index < itemCount - 1)
+        Divider(
+          indent: 50,
+          height: 0,
+        ),
+
+      ],
+    );
+    //   Column(
+    //   children: [
+    //     Row(
+    //           children: [
+    //             WidgetUtils.commonContainer(
+    //               margin: WidgetUtils.edgeInsetsOnly(left: 10,top: 10),
+    //                 decoration: WidgetUtils.commonBoxDecoration(
+    //                     color: AppConstant.whiteColor,
+    //                     border: Border.all(
+    //                       color: AppConstant.whiteColor,
+    //                     )),
+    //                 child: Icon(icon)),
+    //                 Expanded(
+    //                   child: WidgetUtils.commonContainer(
+    //                     // padding: WidgetUtils.edgeInsetsOnly(left: 10,top: 10),
+    //                     decoration: WidgetUtils.commonBoxDecoration(
+    //                       border: Border(
+    //                         bottom: BorderSide(color: AppConstant.greyColor.withOpacity(0.5),width: 1,)
+    //                       )
+    //                     ),
+    //                     child: WidgetUtils.commonTextWidget(
+    //                       text: text,
+    //                       textColor: AppConstant.blackColor.withOpacity(0.5),
+    //                       fontSize: 18,
+    //                     ),
+    //                   ),
+    //                 )
+    //           ],
+    //         ),
+    //     // Divider(
+    //     //   indent: 45,
+    //     // )
+    //   ],
+    // );
   }
 }
