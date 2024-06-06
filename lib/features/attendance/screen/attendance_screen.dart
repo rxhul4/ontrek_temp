13c9b8a326e3b36950a96a59ce4457d1a82c01e5
kuldeep <kuldeep.chauhan.epistic@gmail.com>
2024-06-06 21:54:05 +0530

    done

--- lib/features/attendance/screen/attendance_screen.dart
+++ lib/features/attendance/screen/attendance_screen.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_jailbreak_detection/flutter_jailbreak_detection.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:interval_time_picker/interval_time_picker.dart';
